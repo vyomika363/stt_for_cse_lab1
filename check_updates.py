dd792fba7670a22c26ff5b1cd20b1c1efc669ed4
Vyomika <143392232+vyomika363@users.noreply.github.com>
2025-08-30 18:45:05 +0530

    Update check_updates.py

--- check_updates.py
+++ check_updates.py
@@ -1,2 +1,31 @@
 print("Hello world")
 print("This is STT for CSE lab 1")
+
+num1 = 10
+num2 = 14
+num3 = 12
+
+def greatest_num(a,b,c):
+  if a >= b and a >= c:
+    return a
+  elif b >= a and b >= c:
+    return b
+  else:
+    return c
+
+def smallest_num(a,b,c):
+  if a <= b and a <= c:
+    return a
+  elif b <= a and b <= c:
+    return c
+  else:
+    return c
+
+#Now we have defined 2 functions which will return the greatest and smallest values amongst 3 numbers
+#They take int inputs and return int outputs
+
+largest= greatest(num1,num2,num3)
+smallest=smallest_num(num1,num2,num3)
+
+print("greatest number here is ", largest)
+print("smallest number here is ", smallest)

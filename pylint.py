890452ea6ce8905f3d53a9d00ee06b5f54e8f828
Vyomika <143392232+vyomika363@users.noreply.github.com>
2025-08-30 19:17:44 +0530

    Update pylint.py

--- pylint.py
+++ pylint.py
@@ -1,3 +1,4 @@
+"This is a function to greet, and then print the greatest and smallest values"
 print("Hello world")
 print("This is STT for CSE lab 1")
 
@@ -6,26 +7,26 @@ NUM_2= 14
 NUM_3= 12
 
 def greatest_num(a,b,c):
+    "This is a function for the greatest value."
     if a >= b and a >= c:
         return a
     if b >= a and b >= c:
         return b
-    else:
-        return c
+    return c
 
 def smallest_num(a,b,c):
+    "This is a function for the smallest value."
     if a <= b and a <= c:
         return a
     if b <= a and b <= c:
-        return c
-    else:
-        return c
+        return b
+    return c
 
-#Now we have defined 2 functions which will return the greatest and smallest    
+#Now we have defined 2 functions which will return the greatest and smallest
 #They take int inputs and return int outputs
 
-largest= greatest_num(NUM_1,NUM_2,NUM_3)
-smallest=smallest_num(NUM_1,NUM_2,NUM_3)
+L= greatest_num(NUM_1,NUM_2,NUM_3)
+S=smallest_num(NUM_1,NUM_2,NUM_3)
 
-print("greatest number here is ", largest)
-print("smallest number here is ", smallest)
+print("greatest number here is ", L)
+print("smallest number here is ", S)
